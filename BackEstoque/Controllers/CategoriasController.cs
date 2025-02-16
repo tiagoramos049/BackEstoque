71aee089840dd0064077a72aab27ffdf96f45f96
Tiago <tiagoramos049@gmail.com>
2025-02-16 20:27:47 -0300

    nova

--- BackEstoque/Controllers/CategoriasController.cs
+++ BackEstoque/Controllers/CategoriasController.cs
@@ -14,6 +14,7 @@ namespace BackEstoque.Controllers
         public CategoriasController(ICategoriaRepository categoriaRepository)
         {
             _categoriaRepository = categoriaRepository;
+
         }
 
 
